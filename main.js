24235e652c7b89fb2a274b70a6bfa8ca06761ea4
unknown <FP0023@FoxwellPower.Local>
2023-04-20 09:34:19 +0800

    init

---
+++ main.js
@@ -0,0 +1,116 @@
+;(function (window, document) {
+    let todoList = []
+    const todoListDOM = document.getElementById('todoList')
+    const todoInputDOM = document.getElementById('todoInput')
+    const addBtn = document.getElementById('add')
+
+    // 把 Todo 資料渲染畫面
+    function renderTodoList(todoList) {
+        const html = todoList
+            .map(
+                (item, index) => `
+                <div class="list-content" data-id="${item.id}">
+                    <img class="button-img toggle-btn" src="${item.isComplete ? '/src/assets/img/check.png' : '/src/assets/img/circle-outline.png'}" alt="" data-id="${item.id}" />
+                    <div class="${item.isComplete ? 'content content-finish' : 'content content-unfinish'}" data-id="${item.id}">${item.desc}</div>
+                    <img class="button-img delete-btn" src="/src/assets/img/close.png" alt="" data-id="${item.id}" />
+                </div>
+            `
+            )
+            .join('')
+        todoListDOM.innerHTML = html
+    }
+
+    // 取得待辦事項清單（GET）
+    function getTodos() {
+        console.log('發送 API 抓取資料')
+        fetch('http://localhost:3000/todolist')
+            .then((res) => res.json())
+            .then((json) => {
+                todoList = todoList.concat(json)
+                renderTodoList(todoList) // render todoList
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+        console.log('成功抓取 API 資料')
+    }
+    getTodos()
+    console.log('Next')
+
+    // Post New Todo 事件
+    function postNewTodo(value) {
+        const postData = { desc: value, isComplete: false }
+        fetch('http://localhost:3000/todolist', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(postData),
+        })
+            .then((res) => res.json())
+            .then((json) => {
+                console.log('json', json)
+                todoList.push(json)
+                renderTodoList(todoList)
+                todoInputDOM.value = '' // 清空輸入框
+            })
+    }
+    // 監聽 input 的 keydown(enter) 事件，觸發 postNewTodo()
+    todoInputDOM.addEventListener('keydown', (event) => {
+        if (event.keyCode === 13 && event.target.value) {
+            postNewTodo(event.target.value)
+        }
+    })
+    // 監聽 add Btn 的 click 事件，觸發 postNewTodo()
+    addBtn.addEventListener('click', (event) => {
+        if (todoInputDOM.value) {
+            postNewTodo(todoInputDOM.value)
+        }
+    })
+
+    // Put Todo 事件 + DELETE Todo 事件
+    // Put Todo 事件
+    function putTodo(id) {
+        let postData = {}
+        todoList.forEach((item) => {
+            if (Number(item.id) === Number(id)) {
+                // 切換『已完成』和『未完成』狀態
+                item.isComplete = !item.isComplete
+                postData = item
+            }
+        })
+        fetch(`http://localhost:3000/todolist/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(postData),
+        })
+            .then((res) => res.json())
+            .then((json) => {
+                renderTodoList(todoList)
+            })
+    }
+    // DELETE Todo 事件
+    function deleteTodo(id) {
+        fetch(`http://localhost:3000/todolist/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((res) => res.json())
+            .then((json) => {
+                todoList = todoList.filter((item) => Number(item.id) !== Number(id))
+                renderTodoList(todoList)
+            })
+    }
+    // 監聽 toggle Btn 與 delete btn 的 click 事件，觸發 putTodo() 或 deleteTodo()
+    todoListDOM.addEventListener('click', (event) => {
+        if (event.target.classList.contains('toggle-btn')) {
+            putTodo(parseInt(event.target.dataset.id))
+        } else if (event.target.classList.contains('delete-btn')) {
+            deleteTodo(parseInt(event.target.dataset.id))
+        }
+    })
+})(window, document)
---
+++ sw.js
@@ -0,0 +1,69 @@
+const filesToCache = ['/', '/src/main.css', '/src/assets/img/checklist.png', '/src/assets/img/plus.png', '/src/assets/img/check.png', '/src/assets/img/circle-outline.png', '/src/assets/img/close.png', '/index.html']
+const cacheName = 'todolist-v1'
+
+// install
+// 在 install 裡面要透過 Cache Storage（非同步）把所有靜態檔案 Cache 到瀏覽器中
+self.addEventListener('install', (event) => {
+    console.log('installing…')
+    event.waitUntil(
+        // 使用 CacheStorage(caches) 的 open 方法來取得 cache 物件，如果瀏覽器內原本就有儲存，則會取得原本的，如果沒有則會建立一個新的 cache 物件並取得。
+        caches.open(cacheName).then((cache) => {
+            console.log('Caching app ok, cache:', cache)
+
+            // Cache 物件的 addAll 方法，會傳入 request URL 的陣列並返回一個 Promise 物件。
+            // cache.addAll 會去取得所有 URL 的 response object 並儲存到該 cache 中，把 URL 當作 key，response object 當作 value。
+            return cache.addAll(filesToCache)
+        })
+    )
+})
+
+// activate
+// 為了避免 Fetch 到的資料會一直被 Cache 住，不會自動更新，所以我們必須清除舊的 Cache。
+self.addEventListener('activate', (event) => {
+    console.log('now ready to handle fetches!')
+    event.waitUntil(
+        // caches.keys() 是 caches 的 API，負責的工作是把所有的 cacheName 取出來，回傳的 cacheNames 是個 Array，是一個 Array、裡面的元素是 String。
+        caches.keys().then(function (cacheNames) {
+            // cacheNames.map 產生 Array，map 出來的 item 去判斷是不是等於 cacheName，如果不等於就刪除 cached 檔案。
+            const promiseArr = cacheNames.map(function (item) {
+                if (item !== cacheName) {
+                    return caches.delete(item) // Delete that cached file
+                }
+            })
+            return Promise.all(promiseArr)
+        })
+    )
+})
+// 在使用 service worker 時，activate event 會在以下幾種情況下被觸發：
+// 1. 新版本的 service worker 安裝完成後，之前的 service worker 就會進入到 activate 狀態。這樣可以確保新的 service worker 確實取代了舊的 service worker。
+// 2. 在瀏覽器關閉再重新啟動時，瀏覽器會重新啟動 service worker。這樣可以確保 service worker 在重新啟動後能夠運行。
+// 3. 在 service worker 的檔案內容有任何更動時，例如改變 cache 名稱、更新 cache 內容等等，都會觸發 activate event。
+// 在 activate event 中，可以進行一些資源釋放、cache 清理等等操作，以確保新的 service worker 正確運行。例如，可以清除舊的 cache，避免佔用過多的磁碟空間，同時也可以加載新的 cache，以保證在下次網頁請求時緩存資源正確地更新。
+
+// fetch
+// 第一次進入網站，因為 Service Worker 還沒有被註冊，而我們的 request 會在 Service Worker 註冊完成進入到 activate 狀態之前就發送，所以通常第一次進入網站不會攔截到任何的 fetch 事件。
+self.addEventListener('fetch', (event) => {
+    // console.log('now fetch!')
+    // console.log('event.request:', event.request)
+    // console.log('[ServiceWorker] Fetch', event.request.url)
+
+    const dataUrl = 'http://localhost:3000'
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            // 如果存在 cached 過的 response 資料，則將其回傳
+            // 否則，使用 fetch 把 HTTP request 真的送出，在收到 response 後存到 cache，再回傳
+            return (
+                response ||
+                fetch(event.request).then((res) =>
+                    // 存 caches 之前，要先打開 caches.open(dataCacheName)
+                    caches.open(cacheName).then(function (cache) {
+                        console.log('fetch event.request', event.request)
+                        // cache.put(key, value) 下一次 caches.match 會對應到 event.request
+                        cache.put(event.request, res.clone())
+                        return res
+                    })
+                )
+            )
+        })
+    )
+})
